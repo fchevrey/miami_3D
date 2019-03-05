0fece910e7ebc660e32f24003721d90b0a46dd78
Fabien CHEVREY <fchevrey@student.42.fr>
2019-03-05 18:45:01 +0100

    debug TGA parser

--- srcs/tga/parse_tga_ok.c
+++ srcs/tga/parse_tga_ok.c
@@ -27,7 +27,7 @@ unsigned char		*ft_init_image(int fd, t_header *header)
 		malloc_failed("tga image\n"); //malloc error
 ; // malloc failed
 	rd = read(fd, image, size);
-	//image[rd] = '\0';
+	image[rd] = '\0';
 	return (image);
 }
 
@@ -40,21 +40,32 @@ void			ft_fill_image(t_texture *img, unsigned char *image, t_header *header)
 
 	x = 0;
 	y = 0; // * 4;
-	z = ((header->y - 1) * (header->x)) * 4;
+	z = ((header->y -1) * (header->x)) * 4;
+	//z = ((header->y) * (header->x)) * 4;
+	printf("z = %d y = %d\n", z, y);
 	i = 0;
-	while (i < ((header->x * header->y + 1)))
+	while (i < ((header->x * header->y)))
 	{
 		if (x > header->x -1)
 		{
 			x = 0;
 			y++;
-			z = ((header->y - (y + 1)) * header->x) * 4;
+			z = ((header->y - (y + 1)) * (header->x)) * 4;
 		}
 		pt_to_tex((t_point){x, y}, img, get_color(image[z + (x * 4)], image[z + (x * 4) + 1], image[z + (x * 4) + 2], image[z + (x * 4) + 3]));
+		if (get_pixel(pt_set(x, y), img) == (uint32_t)4278190080)
+		{
+			printf("i = %d  (%d, %d) => color = %08x\n", i, x, y, get_pixel(pt_set(x, y), img));
+			printf("get_color = %08x\n", get_color(image[z + (x * 4) ], image[z + (x * 4) +1], image[z + (x * 4) + 2], image[z + (x * 4) + 3]));
+		}
+		if(i < 5 || i == 63)
+			printf("i = %d, get_color = %08x\n", i, get_color(image[z + (x * 4)], image[z + (x * 4) + 1], image[z + (x * 4)+2], image[z + (x * 4) + 3]));
 		x++;
 		i++;
 		//i += 4;
 	}
+	//printf("i = %d\n", i -1);
+	//printf("0,0 = 0x%08x\n, 63,63 = %u   lenght = %d, %d\n", get_pixel(pt_set(0, 0), img), get_pixel(pt_set(63,63), img), img->size.x, img->size.y);
 }
 /*
 void			ft_fill_image(t_texture *img, unsigned char *image, t_header *header)
@@ -92,6 +103,15 @@ void			fill_real_size(t_header *header)
 	header->y = y.value;
 	printf("largeur image en pixel = %d, hauteur image en pixels = %d\n",header->x, header->y);
 }
+void			print_header(t_header *header)
+{//
+	printf("HEADER\n");
+	printf("---\nid_lenght = %u\n", header->id_length);
+	printf("colormap = %u\n", header->color_map_type);
+	printf("image type = %u\n", header->image_type);
+	printf("x = %hd\n", header->x);
+	printf("y = %hd\n---\n", header->y);
+}
 
 void			ft_load_texture(int *endian, char *str, t_texture *img)
 {
@@ -113,6 +133,7 @@ void			ft_load_texture(int *endian, char *str, t_texture *img)
 	image = ft_init_image(fd, &header);
 	image = ft_decode_tga(endian, &header, image);
 	ft_fill_image(img, image, &header);
+	print_header(&header);
 	free(image);
 	close(fd);
 }
