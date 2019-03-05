a8ac9926060cb3f778a5daa35c14680a3268e8ba
Fabien CHEVREY <fchevrey@student.42.fr>
2019-03-05 19:25:03 +0100

    norminette tga

--- srcs/tga/decode_tga.c
+++ srcs/tga/decode_tga.c
@@ -1,23 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   decode_tga.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fchevrey <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/05 18:57:18 by fchevrey          #+#    #+#             */
+/*   Updated: 2019/03/05 18:59:16 by fchevrey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# include "parse.h"
+#include "parse.h"
 
-static void		find_endian(int *endian)
+static void				find_endian(int *endian)
 {
 	int		s;
 	char	*c;
 
 	s = 1;
-	c = (char*) &s;
+	c = (char*)&s;
 	if (*c == 1)
 		*endian = 1;
 	else
 		*endian = 0;
-	printf("FIND ENDIAN - endian = %d\n", *endian);
 }
 
-static unsigned char	*ft_decode_tga_little_endian(t_header *header, unsigned char *image)
+static unsigned char	*ft_decode_tga_little_endian(t_header *header,
+		unsigned char *image)
 {
-	unsigned char 	*end;
+	unsigned char	*end;
 	unsigned char	*begin;
 	unsigned char	argb[4];
 
@@ -35,13 +46,13 @@ static unsigned char	*ft_decode_tga_little_endian(t_header *header, unsigned cha
 		image[3] = argb[3];
 		image += 4;
 	}
-	printf("LITTLE ENDIAN\n");
 	return (begin);
 }
 
-static unsigned char	*ft_decode_tga_big_endian(t_header *header, unsigned char *image)
+static unsigned char	*ft_decode_tga_big_endian(t_header *header,
+		unsigned char *image)
 {
-	unsigned char 	*end;
+	unsigned char	*end;
 	unsigned char	*begin;
 	unsigned char	argb[4];
 
@@ -59,20 +70,19 @@ static unsigned char	*ft_decode_tga_big_endian(t_header *header, unsigned char *
 		image[0] = argb[3];
 		image += 4;
 	}
-	printf("BIG ENDIAN\n");
 	return (begin);
 }
 
-unsigned char			*ft_decode_tga(int *endian, t_header *header, unsigned char *image)
+unsigned char			*ft_decode_tga(int *endian, t_header *header,
+		unsigned char *image)
 {
-	unsigned char 	*end;
+	unsigned char	*end;
 	unsigned char	*begin;
 
 	begin = image;
 	end = image + (header->x * header->y * 4);
 	if (*endian == -1)
 		find_endian(endian);
-	printf("DECODE - endian = %d\n", *endian);
 	if (*endian == 0)
 		begin = ft_decode_tga_little_endian(header, image);
 	else
--- srcs/tga/parse_tga.c
+++ srcs/tga/parse_tga.c
@@ -1,37 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_tga.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fchevrey <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/05 18:57:28 by fchevrey          #+#    #+#             */
+/*   Updated: 2019/03/05 19:23:04 by fchevrey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# include "parse.h"
+#include "parse.h"
 
-void            ft_fill_header(t_header *header, char *buff, int i)
+static int				ft_fill_header(t_header *header, int fd)
 {
-	if (i == 0)
-		header->id_length = (unsigned char)*buff;
-	if (i == 1)
-		header->color_map_type = (unsigned char)*buff;
-	if (i == 2)
-		header->image_type = (unsigned char)*buff;
-	if (i > 2 && i < 8)
-		header->color_map_spec[i - 3] = (unsigned char)*buff;
-	if (i >= 8 && i < 18)
-		header->image_spec[i - 8] = (unsigned char)*buff;
+	char	buff[18];
+	int		i;
+
+	if (read(fd, buff, 18) < 18)
+		return (-1);
+	header->id_length = (unsigned char)buff[0];
+	header->color_map_type = (unsigned char)buff[1];
+	header->image_type = (unsigned char)buff[2];
+	i = 2;
+	while (i < 18)
+	{
+		if (i < 8)
+			header->color_map_spec[i - 3] = (unsigned char)buff[i];
+		else
+			header->image_spec[i - 8] = (unsigned char)buff[i];
+		i++;
+	}
+	return (0);
 }
 
-unsigned char		*ft_init_image(int fd, t_header *header)
+static unsigned char	*ft_init_image(int fd, t_header *header)
 {
-	unsigned char *image;
-	int size;
-	int rd;
+	unsigned char	*image;
+	int				size;
+	int				rd;
 
 	image = NULL;
-	size = header->x * header->y * 4;
+	size = (header->x) * (header->y) * 4;
 	if (!(image = (unsigned char *)malloc(sizeof(unsigned char) * size + 1)))
-		malloc_failed("tga image\n"); //malloc error
-; // malloc failed
+		malloc_failed("tga image\n");
 	rd = read(fd, image, size);
-	//image[rd] = '\0';
 	return (image);
 }
 
-void			ft_fill_image(t_texture *img, unsigned char *image, t_header *header)
+static void				ft_fill_image(t_texture *img, unsigned char *image,
+		t_header *header)
 {
 	int			x;
 	int			y;
@@ -39,47 +57,26 @@ void			ft_fill_image(t_texture *img, unsigned char *image, t_header *header)
 	int			z;
 
 	x = 0;
-	y = 0; // * 4;
+	y = 0;
 	z = ((header->y - 1) * (header->x)) * 4;
 	i = 0;
 	while (i < ((header->x * header->y + 1)))
-	{
-		if (x > header->x -1)
-		{
-			x = 0;
-			y++;
-			z = ((header->y - (y + 1)) * header->x) * 4;
-		}
-		pt_to_tex((t_point){x, y}, img, get_color(image[z + (x * 4)], image[z + (x * 4) + 1], image[z + (x * 4) + 2], image[z + (x * 4) + 3]));
-		x++;
-		i++;
-		//i += 4;
-	}
-}
-/*
-void			ft_fill_image(t_texture *img, unsigned char *image, t_header *header)
-{
-	int x;
-	int y;
-	int i;
-
-	y = 0;
-	i = 0;
-	x = 0;
-	while (i < header->x * header->y * 4)
 	{
 		if (x > header->x - 1)
 		{
 			x = 0;
 			y++;
+			z = ((header->y - (y + 1)) * header->x) * 4;
 		}
-		pt_to_tex((t_point){x, y}, img, get_color(image[i + 3], image[i], image[i + 1], image[i + 2]));
+		pt_to_tex(pt_set(x, y), img, get_color(image[z + (x * 4)],
+					image[z + (x * 4) + 1], image[z + (x * 4) + 2],
+					image[z + (x * 4) + 3]));
 		x++;
-		i += 4;
+		i++;
 	}
-}*/
+}
 
-void			fill_real_size(t_header *header)
+static void				fill_real_size(t_header *header)
 {
 	t_fill x;
 	t_fill y;
@@ -90,24 +87,23 @@ void			fill_real_size(t_header *header)
 	y.val.b = header->image_spec[7];
 	header->x = x.value;
 	header->y = y.value;
-	printf("largeur image en pixel = %d, hauteur image en pixels = %d\n",header->x, header->y);
 }
 
-void			ft_load_texture(int *endian, char *str, t_texture *img)
+void					ft_load_texture(int *endian, char *str, t_texture *img)
 {
 	int					fd;
-	char				buff[1];
 	unsigned char		*image;
-	int					rd;
-	int					i;
-	t_header	 		header;
+	t_header			header;
 
 	if ((fd = open(str, O_RDONLY)) == -1)
 		return ;
-	i = 0;
-	while (i < 18 && (rd = read(fd, buff, 1)) > 0)
-		ft_fill_header(&header, buff, i++);
-	if (header.id_length != 0 && header.color_map_type != 0 && header.image_type != 2)
+	if (ft_fill_header(&header, fd) < 0)
+	{
+		close(fd);
+		return ;
+	}
+	if (header.id_length != 0 && header.color_map_type != 0
+			&& header.image_type != 2)
 		return ;
 	fill_real_size(&header);
 	image = ft_init_image(fd, &header);
